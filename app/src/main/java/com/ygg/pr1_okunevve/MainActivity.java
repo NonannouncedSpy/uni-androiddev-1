9af610852c8ac779d9aeca44dc34038516fe8408
Ygg <gex_klo@mail.ru>
2025-02-15 10:27:07 +0300

    Added java code for the button and popup. Also put some static strings into strings.xml

--- app/src/main/java/com/ygg/pr1_okunevve/MainActivity.java
+++ app/src/main/java/com/ygg/pr1_okunevve/MainActivity.java
@@ -1,7 +1,15 @@
 package com.ygg.pr1_okunevve;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +28,49 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        /* making the button actually do smth with a listener
+        in this case, it checks input field's text and passes the appropriate "response" for the popup
+        */
+        final Button verifyButton = findViewById(R.id.button_verify);
+        verifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText inputField = findViewById(R.id.edittext_prompt);
+                String popupText = (inputField.getText().toString().equalsIgnoreCase("make a lotta money drive a fancy car"))?
+                        "Lario" : ">:(";
+
+                processPopup(v, popupText);
+            }
+        });
+    }
+
+    private void processPopup(View view, String text) {
+        //most of the following code was roughly copied from the Inters Net, because i uhh am not too experienced in this :)
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup, null);
+
+        //some window settings + the window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        //get into the window and modify le text
+        TextView popup_text = findViewById(R.id.popup_fromButton);
+        popup_text.setText(text);
+
+        //show the popup
+        popupWindow.showAtLocation(view, Gravity.CENTER, width, height);
+
+        //make it go away on any tap
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
         });
     }
 }
