4b872cfc69bb2f88299cdff3ce482b9310f2d0c4
Ygg <gex_klo@mail.ru>
2025-02-15 11:35:32 +0300

    Fixed code not working at all lol :) Modified both main and popup layouts a little. Changed app icon.

--- app/src/main/java/com/ygg/pr1_okunevve/MainActivity.java
+++ app/src/main/java/com/ygg/pr1_okunevve/MainActivity.java
@@ -1,10 +1,12 @@
 package com.ygg.pr1_okunevve;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -46,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void processPopup(View view, String text) {
+    public void processPopup(View view, String text) {
         //most of the following code was roughly copied from the Inters Net, because i uhh am not too experienced in this :)
 
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
@@ -58,11 +60,13 @@ public class MainActivity extends AppCompatActivity {
         boolean focusable = true;
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
         //get into the window and modify le text
-        TextView popup_text = findViewById(R.id.popup_fromButton);
+        TextView popup_text = popupView.findViewById(R.id.popup_fromButton);
         popup_text.setText(text);
+        popup_text.setBackgroundColor((text.equalsIgnoreCase("lario")? Color.parseColor("#00cc00") :
+                Color.parseColor("#cc0000")));
 
         //show the popup
-        popupWindow.showAtLocation(view, Gravity.CENTER, width, height);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
         //make it go away on any tap
         popupView.setOnTouchListener(new View.OnTouchListener() {
